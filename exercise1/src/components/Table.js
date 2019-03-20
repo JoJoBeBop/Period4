c88187ec9aa34ce4057333eda304e212ae0ff42c
JoJoBeBop <jori.blom@metropolia.fi>
2019-03-20 18:36:36 +0200

    Keskiviikko Ajax A

---
+++ exercise1/src/components/Table.js
@@ -0,0 +1,51 @@
+//This is a class based component like app.js
+import React, {Component} from 'react';
+import Tableitem from "./Tableitem"
+import Tablebody from "./Tablebody"
+
+import PropTypes from "prop-types";
+
+class Table extends Component {
+
+    render() {
+        console.log(this.props.picArray);
+
+        return this.props.picArray.map ((table, i) => (
+            //            <h3> { table.title }</h3>
+            <Tableitem key={i} table={table}/>
+
+        ));
+    }
+}
+
+Table.propTypes = {
+    table: PropTypes.array.isRequired
+};
+
+export default Table;
+
+/*//This is a class based component like app.js
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import Tablebody from "./Tablebody"
+
+
+class Table extends Component {
+
+    render() {
+        return(
+            <table>
+                <Tablebody />
+            </table>
+        )
+    }
+}
+
+Table.propTypes = {
+    table: PropTypes.array.isRequired
+};
+
+export default Table;
+*/
+
+
